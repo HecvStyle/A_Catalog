95ed69681b94a365615459c7bbb7084e1b8c910e
hecv <hecvstyle@gmail.com>
2016-12-28 14:16:38 +0800

    Initial Commit

---
+++ A_Catalog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  A_Catalog
+//
+//  Created by 何巍 on 16/12/28.
+//  Copyright © 2016年 东田教育. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
